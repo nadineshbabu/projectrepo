7c3913aca50a8011a9d9b85f783bf1f89ec78547
nadineshbabu <30660867+nadineshbabu@users.noreply.github.com>
2018-03-22 11:59:39 +0530

    Create objectrepo.java
    xpaths

---
+++ MasterData/objectrepo.java
@@ -0,0 +1,14 @@
+package frameworkstructure;
+
+public class objerepo {
+
+		public static String HomePage_Youramazon_LK = "//*[@id='nav-your-amazon";
+			String Emailloginfield="//*[@id='ap_email']";
+			String continuebutton="//*[@id='continue']";
+			String Errortext= "//*[@id='auth-error-message-box']/div/div/ul/li/span";
+			
+			
+		
+			
+			
+}
